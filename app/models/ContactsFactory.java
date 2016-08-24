6a9ea6c00087bd176133e3cbc5e93cadd956bed1
Joshua Paylaga <joshuapaylaga@gmail.com>
2016-08-25 00:39:22 +0800

    Completed ContactsFactory and fix JPA issue

--- app/models/ContactsFactory.java
+++ app/models/ContactsFactory.java
@@ -1,6 +1,7 @@
 package models;
 
 import java.util.*;
+import java.text.*;
 
 import javax.ws.rs.client.*;
 import javax.ws.rs.core.MediaType;
@@ -8,21 +9,105 @@ import org.simpleframework.xml.*;
 import org.simpleframework.xml.core.*;
 import org.simpleframework.xml.transform.*;
 
+import models.jpa.Contact;
+
+import models.xml.*;
+import models.xml.DateFormatTransformer;
+
 public class ContactsFactory {
 
-    public static void create(String username, String apiKey) {
+    public static List<Contact> create(String username, String apiKey) {
+        People people = null;
+        List<Contact> contacts = new ArrayList<Contact>();
+
+        // Parse XML from highrise API
         try {
             String basicAuth = Base64.getEncoder().encodeToString((apiKey + ":x").getBytes("utf-8"));
-            String apiUrl    = "https://" + username + ".highrisehq.com";
+            String apiUrl = "https://" + username + ".highrisehq.com";
             Client client = ClientBuilder.newClient();
             String entity = client.target(apiUrl)
                             .path("people.xml")
                             .request(MediaType.APPLICATION_XML)
                             .header("authorization", "Basic " + basicAuth)
                             .get(String.class);
-            System.out.println(entity);
+
+            DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+            RegistryMatcher matcher = new RegistryMatcher();
+            matcher.bind(Date.class, new DateFormatTransformer(format));
+            Serializer serializer = new Persister(matcher);
+            people = serializer.read(People.class, entity);
         } catch (Exception e) {
             System.out.println(e);
         }
+
+        // Put parsed XML to contacts
+        if (people != null) {
+            for (Person person : people.getPersons()) {
+                contacts.add(parsePersonToContact(person));
+            }
+        }
+
+        return contacts;
+    }
+
+    private static Contact parsePersonToContact(Person person) {
+        Contact contact = new Contact(
+            person.getFirstName(),
+            person.getLastName(),
+            person.getCreatedAt(),
+            person.getUpdatedAt()
+        );
+
+        contact.setTitle(person.getTitle());
+        contact.setCompanyName(person.getCompanyName());
+        contact.setBackground(person.getBackground());
+        contact.setLinkedinUrl(person.getLinkedinUrl());
+        contact.setInstantMessengerProtocol(
+            (person.getContactData().getInstantMessengers().size() > 0)
+            ? person.getContactData().getInstantMessengers().get(0).getProtocol()
+            : null
+        );
+        contact.setInstantMessengerAddress(
+            (person.getContactData().getInstantMessengers().size() > 0)
+            ? person.getContactData().getInstantMessengers().get(0).getAddress()
+            : null
+        );
+        contact.setEmailAddress(
+            (person.getContactData().getEmailAddresses().size() > 0)
+            ? person.getContactData().getEmailAddresses().get(0).getAddress()
+            : null
+        );
+        contact.setTwitterAccount(
+            (person.getContactData().getTwitterAccounts().size() > 0)
+            ? person.getContactData().getTwitterAccounts().get(0).getUsername()
+            : null
+        );
+        contact.setAddress(
+            (person.getContactData().getAddresses().size() > 0)
+            ? addressToString(person.getContactData().getAddresses().get(0))
+            : null
+        );
+        contact.setWebAddress(
+            (person.getContactData().getWebAddresses().size() > 0)
+            ? person.getContactData().getWebAddresses().get(0).getUrl()
+            : null
+        );
+
+        for (Tag tag : person.getTags()) {
+            contact.addTag(tag.getName());
+        }
+
+        return contact;
+    }
+
+    private static String addressToString(Address address) {
+        String sep = ", ";
+        String strAddress = address.getStreet() + sep +
+                            address.getCity() + sep +
+                            address.getState() + sep +
+                            Integer.toString(address.getZip()) + sep +
+                            address.getCountry();
+
+        return strAddress;
     }
 }
--- test/JpaTest.java
+++ test/JpaTest.java
@@ -2,6 +2,7 @@ import org.junit.*;
 import java.util.*;
 import play.test.*;
 import models.jpa.*;
+import models.ContactsFactory;
 
 public class JpaTest extends UnitTest {
 
@@ -110,5 +111,28 @@ public class JpaTest extends UnitTest {
 
         assertEquals(2, contact1.getTags().size());
         assertEquals(1, contact2.getTags().size());
+    }
+
+    @Test
+    public void createContactsFromXML() {
+        List<Contact> contacts = ContactsFactory.create("joshuapaylaga", "2b67f902fd7566e5c1cfdecc4969aa98");
+        assertEquals(2, contacts.size());
+        assertEquals("Marron Christopher", contacts.get(0).getFirstName());
+        assertEquals("Afable", contacts.get(0).getLastName());
+        assertEquals("Kris Ann", contacts.get(1).getFirstName());
+        assertEquals("Bachinela", contacts.get(1).getLastName());
+
+        Contact krisAnn = contacts.get(1);
+        assertEquals("Student", krisAnn.getTitle());
+        assertEquals("Misamis University", krisAnn.getCompanyName());
+        assertEquals("My girlfriend.", krisAnn.getBackground());
+        assertEquals("https://ph.linkedin.com/in/joshua-paylaga-76959b6b", krisAnn.getLinkedinUrl());
+        assertEquals("Yahoo", krisAnn.getInstantMessengerProtocol());
+        assertEquals("kselann", krisAnn.getInstantMessengerAddress());
+        assertEquals("krisannbachinela@gmail.com", krisAnn.getEmailAddress());
+        assertEquals("kidongladz", krisAnn.getTwitterAccount());
+        assertEquals("San Roque, Panaon, Misamis Oriental, 7205, Philippines", krisAnn.getAddress());
+        assertEquals("http://krisannbachinela.blogspot.com/2015/11/my-autobiography-am-kris-ann-balt.html", krisAnn.getWebAddress());
+        assertEquals(1, krisAnn.getTags().size());
     }
 }
